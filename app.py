aecdbf1a09d55d14ecc8dd850a571835e2a3aa5e
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-23 07:44:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 
+print('''
+𝔸𝕟𝕖́𝕚𝕤 𝕞𝕒́𝕘𝕚𝕔𝕠𝕤
+''')
+
+
+print('1. Cadastrar cliente')
+print('2. Listar cliente')
+print('3. Ativar cliente')
+print('4.Sair')
+
+opcao_escolhida = input ('Escolha uma opção:')
+print('Você Escolheu a opção: opcao_escolhida')
