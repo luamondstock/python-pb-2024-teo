f1e7b21e6f06892bfc1e59597885f6395f241794
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-28 11:27:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import os
+
+livros = ('livro': 'Imperfeitos', 'genero': 'Young adult', 'ativo': True),
+        ('livro': 'Divergente', 'genero': 'Distopia', 'ativo': False),
+        ('livro': 'Pai Rico Pai Pobre', 'genero': 'Auto-ajuda', 'ativo': True),
+
 def mostra_titulo():
     print('''
 
@@ -17,32 +22,53 @@ def mostra_escolhas():
     print('4.Sair')
 
 def escolhe_opcao():
-    try:
-        opcao_escolhida = int(input('Escolha uma opção:'))
+        
+        def exibir_subtitulo(texto)
+            os.system('cls')
+            print(texto)
+            print ('')
 
+        def retorna_menu():
+            input(' Digite uma tecla para voltar ao menu principal ')
+            main()
+            
+
+    
         def cadastra_cliente():
             os.system('cls'),
             print('Cadastro de novos clintes\n')
             nome_cliente = input('Digite o nome do cliente que deseja cadastrar')
             clientes.append(nome_cliente)
             print(f'O cliente {'nome_cliente'} foi cadastrado com sucesso\n')
-            input(' Digite uma tecla para voltar ao menu principal ')
+            retorna_menu()
+
+        def listar_livro():
+             exibir_subtitulo('Lista de alunos cadastrados')
+            for livro in clientes
+                print (f'--(aluno)')
+
+            input (' Digite uma tecla para voltar ao menu principal')
             main()
-            
+
         
+    
         def finaliza_programa():
             os.system("cls")
-            print('Finalizando o programa')
+            print('Finalizando o programa\n')
 
         def opcao_invalida():
             print('Opa, este caracter não é permitido\n')
             input('Aperte qualquer tecla para voltar')
             main()
+    
+    try:
+        opcao_escolhida = int(input('Escolha uma opção:'))
+
 
         if opcao_escolhida == 1:
             print('Você escolheu Cadastrar cliente')
         elif opcao_escolhida == 2:
-            print('Você esacolheu Listar livro')
+            print('Você escolheu Listar livro')
         elif opcao_escolhida == 3:
             print('Você escolheu Ativar leitura')
         elif opcao_escolhida == 4:
