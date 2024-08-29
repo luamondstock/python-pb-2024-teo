e70799418ffabe7a578ba33ba7bc51706cd1122a
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-29 08:56:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 
-livros = ('livro': 'Imperfeitos', 'genero': 'Young adult', 'ativo': True),
-        ('livro': 'Divergente', 'genero': 'Distopia', 'ativo': False),
-        ('livro': 'Pai Rico Pai Pobre', 'genero': 'Auto-ajuda', 'ativo': True),
-
+livros = [{'nome': 'Imperfeitos', 'genero': 'Young adult', 'ativo': True},
+          {'nome': 'Divergente', 'genero': 'Distopia', 'ativo': False},
+          {'nome': 'Pai Rico Pai Pobre', 'genero': 'Auto-ajuda', 'ativo': True}]
 def mostra_titulo():
     print('''
 
@@ -16,61 +15,59 @@ def mostra_titulo():
         ''')
 
 def mostra_escolhas():
-    print('1. Cadastrar cliente')
+    print('1. Cadastrar livro')
     print('2. Listar livro')
     print('3. Ativar/Desativar leitura')
     print('4.Sair')
 
 def escolhe_opcao():
         
-        def exibir_subtitulo(texto)
-            os.system('cls')
-            print(texto)
-            print ('')
+    def exibir_subtitulo(texto):
+        os.system('cls')
+        print(texto)
+        print ('')
 
-        def retorna_menu():
-            input(' Digite uma tecla para voltar ao menu principal ')
-            main()
+    def retorna_menu():
+        input(' Digite uma tecla para voltar ao menu principal')
+        main()
             
 
-    
-        def cadastra_cliente():
-            os.system('cls'),
-            print('Cadastro de novos clintes\n')
-            nome_cliente = input('Digite o nome do cliente que deseja cadastrar')
-            clientes.append(nome_cliente)
-            print(f'O cliente {'nome_cliente'} foi cadastrado com sucesso\n')
-            retorna_menu()
+    def cadastra_livro():
+        exibir_subtitulo('Cadastrar livro')
+        nome_livro = input('Digite o nome do livro que deseja cadastrar')
+        livros.append(nome_livro)
+        print(f'O livro {nome_livro} foi cadastrado com sucesso\n')
+        retorna_menu()
 
-        def listar_livro():
-             exibir_subtitulo('Lista de alunos cadastrados')
-            for livro in clientes
-                print (f'--(aluno)')
+    def listar_livro():
+        exibir_subtitulo('Listar de livros cadastrados')
 
-            input (' Digite uma tecla para voltar ao menu principal')
-            main()
-
+        for livro in livros:
+            nome_livro = livro['nome']
+            livro_genero = livro['genero']
+            ativo = livro['ativo']
+            print (f'- {nome_livro} | {livro_genero} | {ativo}')
+        retorna_menu()
         
     
-        def finaliza_programa():
-            os.system("cls")
-            print('Finalizando o programa\n')
+    def finaliza_programa():
+        exibir_subtitulo("Finalizando o programa\n")
+            
 
-        def opcao_invalida():
-            print('Opa, este caracter não é permitido\n')
-            input('Aperte qualquer tecla para voltar')
-            main()
+    def opcao_invalida():
+        print('Opa, este carácter não é permitido\n')
+        retorna_menu()
     
     try:
         opcao_escolhida = int(input('Escolha uma opção:'))
 
 
         if opcao_escolhida == 1:
-            print('Você escolheu Cadastrar cliente')
+            cadastra_livro()
         elif opcao_escolhida == 2:
-            print('Você escolheu Listar livro')
+           listar_livro()
         elif opcao_escolhida == 3:
-            print('Você escolheu Ativar leitura')
+           print('Você escolheu Ativar leitura')
         elif opcao_escolhida == 4:
             finaliza_programa()
         else :
@@ -82,8 +79,6 @@ def main():
     mostra_titulo()
     mostra_escolhas()
     escolhe_opcao()
-opcao_escolhida = int(input('Escolha uma opção:'))
 
 if __name__ == '__main__':
     main()
-
